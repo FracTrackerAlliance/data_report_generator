c5fcb8f62f94250e57819857a6c2b32a82f849ed
twag25 <119757786+twag25@users.noreply.github.com>
2024-10-07 16:36:01 -0500

    added csv drop

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("FracTracker Data Report")
 st.write("This report contains visualizations of various attribute distributions in the data set scraped from Flickr. The data used in this report is scraped from Fractracker's Flickr page found [here](https://www.flickr.com/photos/fractracker/albums/). The photos were captured and labeled by members of the FracTracker organization. These photos contain imagery of fracking sites and sites related to the fracking process across the United States. These photos were taken in support of FracTracker's initiative to document environmental, health-related, and other impacts of fracking.")
+
+st.file_uploader(label = 'Drop the .csv file scraped from Flickr here:', type = 'csv')

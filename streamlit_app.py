03eb144c9c5ec3a2f8acca85769550a9e002665a
twag25 <119757786+twag25@users.noreply.github.com>
2024-10-07 16:04:37 -0500

    updated text on landing page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("FracTracker Data Report")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("This report contains visualizations of various attribute distributions in the data set scraped from Flickr. The data used in this report is scraped from Fractracker's Flickr page found [here](https://www.flickr.com/photos/fractracker/albums/). The photos were captured and labeled by members of the FracTracker organization. These photos contain imagery of fracking sites and sites related to the fracking process across the United States. These photos were taken in support of FracTracker's initiative to document environmental, health-related, and other impacts of fracking.")

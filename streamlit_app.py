a5335f309b1c83997a07540ef43568b7278b12ca
twag25 <119757786+twag25@users.noreply.github.com>
2024-10-07 17:20:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import datetime as dt
 
 st.title("FracTracker Data Report")
 st.write("This report contains visualizations of various attribute distributions in the data set scraped from Flickr. The data used in this report is scraped from Fractracker's Flickr page found [here](https://www.flickr.com/photos/fractracker/albums/). The photos were captured and labeled by members of the FracTracker organization. These photos contain imagery of fracking sites and sites related to the fracking process across the United States. These photos were taken in support of FracTracker's initiative to document environmental, health-related, and other impacts of fracking.")
@@ -7,10 +8,11 @@ st.write("This report contains visualizations of various attribute distributions
 uploaded_file = st.file_uploader(label = 'Drop the .csv file scraped from Flickr here:', type = 'csv')
 
 if uploaded_file is not None:
-    st.write('Data Uploaded Successfully')
-    if st.button(label = 'Download Report'):
-        st.write('clicked')
 
+    df = pd.read_csv(uploaded_file)
+    report_file = # change other data report into an external function - call function from other file here
 
-
-# df = pd.read_csv(uploaded_file)
+    st.download_button(label = "Download Report",
+                       data = report_file,
+                       file_name = 'FracTracker_Data_Report' + str(dt.datetime) + '.')
+        
